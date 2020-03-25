51cae3fbbb3c0d129a3114844ecd2da3b3ba50f1
hilonfot <hilon2019@gmail.com>
2020-03-25 15:14:18 +0800

    fix : catch >>  remove create file

--- utils/catch/panichandler.go
+++ utils/catch/panichandler.go
@@ -3,31 +3,27 @@ package catch
 import (
 	"fmt"
 	"github.com/hilonfot/network/utils/log"
-	"os"
-	"runtime/debug"
-	"time"
+	"runtime"
+	"strings"
 )
 
+func trace(message string) string {
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:]) // skip first 3 caller
+
+	var str strings.Builder
+	str.WriteString(message + "\nTraceback:")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+	return str.String()
+}
+
 func PanicHandler() {
-	exeName := os.Args[0] // 获取程序名称
-
-	now := time.Now()  // 获取当前时间
-	pid := os.Getpid() // 获取进程ID
-
-	timeStr := now.Format("20060102150405")
-	fName := fmt.Sprintf("%s-%d-%s-dump.log", exeName, pid, timeStr)
-	log.Error("dump to file ", fName)
-
-	f, err := os.Create(fName)
-	if err != nil {
-		return
-	}
-	defer f.Close()
-
 	if err := recover(); err != nil {
-		f.WriteString(fmt.Sprintf("%v\r\n", err)) // 输出panic信息
-		f.WriteString("========\r\n")
+		message := fmt.Sprintf("%s", err)
+		log.Errorf("%s\n\n", trace(message))
 	}
-
-	f.WriteString(string(debug.Stack())) //输出堆栈信息
 }
